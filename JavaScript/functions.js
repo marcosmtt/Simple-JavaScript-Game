56d97b569647d6a4745c38ce7580759591d20078
Marcos Motta <marcos.mottabtmn@gmail.com>
2018-01-27 16:47:47 -0200

    Canvas updated

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,5 +1,5 @@
 let canvas;
-let ctx;
+let ctx; //Context
 let HEIGHT, WIDTH;
 let frames = 0;
 
@@ -12,14 +12,24 @@ function main() {
         WIDTH = 800;
     }
 
+    //set game screen size
     canvas = document.createElement("canvas");
     canvas.height = HEIGHT;
     canvas.width = WIDTH;
     canvas.style.border = "2px solid #000";
 
+    ctx = canvas.getContext("2d");
+
+    //Add the Canvas variable to html body
+    document.body.appendChild(canvas);
+
+    document.addEventListener("mousedown", click);
+
 }
 
-function click() { }
+function click(event) {
+    alert("clicou");
+}
 
 function run() { }
 
@@ -29,3 +39,4 @@ function draw() { }
 
 //inicializa o jogo
 main();
+

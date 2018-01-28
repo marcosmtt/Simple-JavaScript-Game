7f2874693e3f64ce296feb54907d310cbaa05411
Marcos Motta <marcos.mottabtmn@gmail.com>
2018-01-28 20:14:33 -0200

    Functions update

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -4,38 +4,48 @@ let HEIGHT, WIDTH;
 let frames = 0;
 
 function main() {
-    HEIGHT = window.innerHeight;
-    WIDTH = window.innerWidth;
+    WIDTH = window.innerWidth; //Largura
+    HEIGHT = window.innerHeight;//Altura
 
     if (HEIGHT >= 500) {
-        HEIGHT = 600;
         WIDTH = 800;
+        HEIGHT = 600;
     }
 
     //set canvas size
     canvas = document.createElement("canvas");
     canvas.height = HEIGHT;
     canvas.width = WIDTH;
-    canvas.style.border = "2px solid #000";
+    canvas.style.border = "5px solid #300";
 
     ctx = canvas.getContext("2d");
 
     //Add the Canvas variable to html body
     document.body.appendChild(canvas);
-
     document.addEventListener("mousedown", click);
 
+    run();
 }
 
 function click(event) {
     alert("clicou");
 }
 
-function run() { }
+function run() {
+    update();
+    draw();
 
-function update() { }
+    window.requestAnimationFrame(run);
+}
 
-function draw() { }
+function update() { 
+    frames++;
+}
+
+function draw() {
+    ctx.fillStyle = "#66c2ff";
+    ctx.fillRect(0,0, WIDTH, HEIGHT);
+ }
 
 //inicializa o jogo
 main();

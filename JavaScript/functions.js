2ef917898d11c09bac659498b731f4a7f15cd7e6
Marcos Motta <marcos.mottabtmn@gmail.com>
2018-01-28 21:50:46 -0200

    Basic Physics and Character Jump added

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -3,6 +3,56 @@ let ctx; //Context
 let HEIGHT, WIDTH;
 let frames = 0;
 
+let ground = {
+    //This variable is used to create the game ground
+    sizeY: 550,
+    height: 50,
+    color: "#803300",
+
+    draw: function () { //método
+        ctx.fillStyle = this.color;
+        ctx.fillRect(0, this.sizeY, WIDTH, this.height);
+    }
+
+};
+
+let character = {
+    //Set basic character parameters and functions
+    x: 50,
+    y: 0,
+    height: 50,
+    width: 50,
+    color: "#ff4e4e",
+
+    //Adding some physics:
+    gravity: 0.8,
+    speed: 1.2,
+    jumpForce: 15,
+
+    update: function () {
+        this.speed += this.gravity;
+        this.y += this.speed;
+
+        if (this.y >= ground.sizeY - this.height) {
+            this.y = ground.sizeY - this.height;
+        }
+    },
+
+    jump: function () {
+        this.speed = -this.jumpForce;
+    },
+
+    draw: function () {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.height, this.width);
+    }
+};
+
+
+function click(event) {
+    character.jump();
+}
+
 function main() {
     WIDTH = window.innerWidth; //Largura
     HEIGHT = window.innerHeight;//Altura
@@ -27,10 +77,6 @@ function main() {
     run();
 }
 
-function click(event) {
-    alert("clicou");
-}
-
 function run() {
     update();
     draw();
@@ -38,14 +84,18 @@ function run() {
     window.requestAnimationFrame(run);
 }
 
-function update() { 
+function update() {
     frames++;
+    character.update();
 }
 
 function draw() {
     ctx.fillStyle = "#66c2ff";
-    ctx.fillRect(0,0, WIDTH, HEIGHT);
- }
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    ground.draw();
+    character.draw();
+}
 
 //inicializa o jogo
 main();

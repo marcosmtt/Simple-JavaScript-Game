0f1c997380fc8490635f53faa8c2e7c9b2219890
Marcos Motta <marcos.mottabtmn@gmail.com>
2018-01-28 22:39:50 -0200

    Initial Character Position defined

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -2,6 +2,7 @@ let canvas;
 let ctx; //Context
 let HEIGHT, WIDTH;
 let frames = 0;
+let character;
 
 let ground = {
     //This variable is used to create the game ground
@@ -16,35 +17,38 @@ let ground = {
 
 };
 
-let character = {
-    //Set basic character parameters and functions
-    x: 50,
-    y: 0,
-    height: 50,
-    width: 50,
-    color: "#ff4e4e",
+function initCharacter(height) {
+    return {
+        //Set basic character parameters and functions
+        height: 50,
+        width: 50,
+        x: 50,
+        y: height / 2 - this.height,
+        color: "#ff4e4e",
 
-    //Adding some physics:
-    gravity: 0.8,
-    speed: 1.2,
-    jumpForce: 15,
 
-    update: function () {
-        this.speed += this.gravity;
-        this.y += this.speed;
+        //Adding some physics:
+        gravity: 0.7,
+        speed: 1.2,
+        jumpForce: 15,
 
-        if (this.y >= ground.sizeY - this.height) {
-            this.y = ground.sizeY - this.height;
+        update: function () {
+            this.speed += this.gravity;
+            this.y += this.speed;
+
+            if (this.y >= ground.sizeY - this.height) {
+                this.y = ground.sizeY - this.height;
+            }
+        },
+
+        jump: function () {
+            this.speed = -this.jumpForce;
+        },
+
+        draw: function () {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.height, this.width);
         }
-    },
-
-    jump: function () {
-        this.speed = -this.jumpForce;
-    },
-
-    draw: function () {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.height, this.width);
     }
 };
 
@@ -57,10 +61,14 @@ function main() {
     WIDTH = window.innerWidth; //Largura
     HEIGHT = window.innerHeight;//Altura
 
+
     if (HEIGHT >= 500) {
         WIDTH = 800;
         HEIGHT = 600;
     }
+
+    character = initCharacter(HEIGHT);
+
 
     //set canvas size
     canvas = document.createElement("canvas");
